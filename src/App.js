bd115bf10a6ac68759c205fc9981969f6e29a880
Raihanul <ronyraihan141@gmail.com>
2023-04-04 13:39:36 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -528,7 +528,7 @@ function App() {
                     My name is Raihanul. I am from Bangladesh. I am a web development enthusiast. 
                     Actually, I am an EEE graduate. I have a professional experience of almost 2.5 years, 
                     but it is not related to Web/Software Development. I have been working on web development
-                     personally (non-professionally) since September 19, 2021. During this time I used React 
+                     personally since September 19, 2021. During this time I used React 
                      JS, Redux, Angular, SASS, LESS, Node JS, Express JS, MongoDB, MySQL, MERN, MEAN,
                       Firebase, EJS Template Engine, JSON Web Token, Multer, Express Validator, Socket.io, 
                       JSON Server, Asynchronous Javascript, Event Loop, Unit Testing, Javascript, Typescript,
